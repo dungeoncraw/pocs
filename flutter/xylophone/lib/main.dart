41b149b9eebec2ae9dfa55cc3e2082bc8a5316c9
Thiago Nascimento <dungeoncraw@gmail.com>
2023-07-06 23:18:52 -0300

    Xylophone app

---
+++ flutter/xylophone/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  Future<void> playSound(int note) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note$note.wav'));
+  }
+
+  Expanded buildKey({Color? color, int note = 1}) {
+    return Expanded(
+      child: MaterialButton(
+        color: color,
+        onPressed: () async {
+          await playSound(note);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, note: 1),
+              buildKey(color: Colors.orange, note: 2),
+              buildKey(color: Colors.yellow, note: 3),
+              buildKey(color: Colors.green, note: 4),
+              buildKey(color: Colors.teal, note: 5),
+              buildKey(color: Colors.blue, note: 6),
+              buildKey(color: Colors.purple, note: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
