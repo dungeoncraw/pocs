d12ea5084a9e33b837be8dcdb531b73262ba3d35
Thiago Nascimento <dungeoncraw@gmail.com>
2024-03-06 11:02:04 -0300

    Init config test for turboflix

---
+++ java/turboflix/app/controllers/HomeController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import play.mvc.*;
+import com.typesafe.config.Config;
+import javax.inject.Inject;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+    // Simple usage of config injection
+    private final Config config;
+
+    // Inject config into class constructor
+    @Inject
+    public HomeController(Config config) {
+        this.config = config;
+    }
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    // this is nice, so to modify the view path folder, follow views.html.FOLDER_NAME.FILENAME.render method
+    public Result index() {
+        return ok(views.html.home.index.render(this.config));
+    }
+
+}
---
+++ java/turboflix/build.sbt
@@ -0,0 +1,10 @@
+name := """turboflix"""
+organization := "com.tnascimento"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.13.13"
+
+libraryDependencies += guice
