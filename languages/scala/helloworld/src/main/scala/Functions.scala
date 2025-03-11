06b5a7be2ff4069871539fa2f6fa643fb81ec5ce
Thiago Nascimento <dungeoncraw@gmail.com>
2025-03-11 19:51:23 -0300

    Lambda and HOC

--- languages/scala/helloworld/src/main/scala/Functions.scala
+++ languages/scala/helloworld/src/main/scala/Functions.scala
@@ -9,6 +9,30 @@ def functions(): Unit = {
   val listUser = List("Amelia", "Maria", "Isadora")
   // splat operator
   sayHello(listUser*)
+
+  // lambdas
+  val myLambda = (a: Int) => a * 2
+  myLambda(10)
+
+  // HOC
+  def myFunction(a: Int, byTwo: Int => Unit): Unit = byTwo(a)
+  myFunction(10, (a: Int) => println(a * 2))
+
+  val clients = List("John", "Mary", "Jane", "Lee")
+  clients.foreach {
+    client => println(s"Hello $client")
+  }
+
+  clients.filter(_.length < 4).foreach(println)
+
+  val lengths = clients.map(_.length)
+  println(lengths)
+
+  // maxBy minBy get the first item that matches the predicate
+  val max = clients.maxBy(_.length)
+  println(max)
+  val min = clients.minBy(_.length)
+  println(min)
 }
 
 // override
