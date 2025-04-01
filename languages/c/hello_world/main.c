dad1513ad04054ee59a2b80dc64ce3ef1cf4921e
Thiago Nascimento <dungeoncraw@gmail.com>
2025-04-01 20:24:09 -0300

    Hello world from C

---
+++ languages/c/hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    printf("This is line 1.\n");
+    printf("This is line 2.\n");
+    printf("Column 1\t Column 2\t Column3 \n");
+    printf("To print backslash use \\\\.\n");
+    printf("To print quotes, use \\\" and \\'.\n");
+    return 0;
+}
