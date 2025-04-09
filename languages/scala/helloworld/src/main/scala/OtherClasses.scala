399801837d9297bee982a5860ab2cf2bd190bbe4
Thiago Nascimento <dungeoncraw@gmail.com>
2025-04-08 21:18:56 -0300

    Some sealed and nested classes

--- languages/scala/helloworld/src/main/scala/OtherClasses.scala
+++ languages/scala/helloworld/src/main/scala/OtherClasses.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 @main
 def otherClasses(): Unit =
   println(s"Hello ${Color.RED}")
@@ -12,6 +14,15 @@ def otherClasses(): Unit =
   Color.RED.drawColor()
   println(Color.BLUE.hex)
 
+  val plant = getPlant()
+  plant match
+    case _: Fruit => println("Fruit")
+    case _: Vegetable => println("Vegetable")
+    case _ => println("Unknown")
+
+  val c = NestedCar()
+  c.drive()
+
 // enums can have constructor and methods!
 enum Color(val hex: String):
   case RED extends Color("0xff0000")
@@ -25,3 +36,32 @@ def decide(color: Color): Unit =
     case Color.RED => println("You choose red")
     case Color.GREEN => println("You choose green")
     case Color.BLUE => println("You choose blue")
+
+// sealed classes can only be accessed by the current package class
+sealed class Plant
+
+sealed class Fruit extends Plant
+
+sealed class Vegetable extends Plant
+
+class Apple extends Fruit
+
+class Potato extends Vegetable
+
+def getPlant(): Plant =
+  if (Random.nextInt(100) % 2 == 0) new Apple() else new Potato()
+
+
+class NestedCar:
+  var speed = 100
+  val engine = Engine()
+
+  def drive(): Unit =
+    engine.start()
+    println(s"Driving at $speed km/h")
+
+  class Engine:
+    val power = 100
+    // this is very dangerous, calling that way the nested class
+    NestedCar.this.speed = 200
+    def start(): Unit = println(s"Starting engine with power $power")
