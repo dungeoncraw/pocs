8e46cd314f8ab9c6b2c9d7060d8905684f159301
Thiago Nascimento <dungeoncraw@gmail.com>
2025-04-11 20:20:48 -0300

    Unsafe queue in rust

--- languages/rust/linked_list/src/lib/fourth.rs
+++ languages/rust/linked_list/src/lib/fourth.rs
@@ -1,13 +1,10 @@
 use std::rc::Rc;
-use std::cell::{Ref, RefCell, RefMut};
+use std::cell::{Ref, RefMut, RefCell};
 
 pub struct List<T> {
     head: Link<T>,
-    tail: Link<T>
+    tail: Link<T>,
 }
-pub struct IntoIter<T>(List<T>);
-
-pub struct Iter<'a, T>(Option<Ref<'a, Node<T>>>);
 
 type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
@@ -16,6 +13,7 @@ struct Node<T> {
     next: Link<T>,
     prev: Link<T>,
 }
+
 
 impl<T> Node<T> {
     fn new(elem: T) -> Rc<RefCell<Self>> {
@@ -32,23 +30,21 @@ impl<T> List<T> {
         List { head: None, tail: None }
     }
 
-    pub fn push_front(&mut self, elem:T) {
+    pub fn push_front(&mut self, elem: T) {
         let new_head = Node::new(elem);
         match self.head.take() {
             Some(old_head) => {
-                // NON-EMPTY LIST
-                // https://doc.rust-lang.org/std/cell/struct.RefCell.html#method.borrow_mut
                 old_head.borrow_mut().prev = Some(new_head.clone());
                 new_head.borrow_mut().next = Some(old_head);
                 self.head = Some(new_head);
             }
             None => {
-                // EMPTY LIST
                 self.tail = Some(new_head.clone());
                 self.head = Some(new_head);
             }
         }
     }
+
     pub fn push_back(&mut self, elem: T) {
         let new_tail = Node::new(elem);
         match self.tail.take() {
@@ -58,14 +54,14 @@ impl<T> List<T> {
                 self.tail = Some(new_tail);
             }
             None => {
-                // new reference to tail
                 self.head = Some(new_tail.clone());
                 self.tail = Some(new_tail);
             }
         }
     }
+
     pub fn pop_back(&mut self) -> Option<T> {
-        self.tail.take().map(|old_tail | {
+        self.tail.take().map(|old_tail| {
             match old_tail.borrow_mut().prev.take() {
                 Some(new_tail) => {
                     new_tail.borrow_mut().next.take();
@@ -78,6 +74,28 @@ impl<T> List<T> {
             Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
         })
     }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                }
+                None => {
+                    self.tail.take();
+                }
+            }
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
+
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+
     pub fn peek_back(&self) -> Option<Ref<T>> {
         self.tail.as_ref().map(|node| {
             Ref::map(node.borrow(), |node| &node.elem)
@@ -95,55 +113,30 @@ impl<T> List<T> {
             RefMut::map(node.borrow_mut(), |node| &mut node.elem)
         })
     }
-    pub fn pop_front(&mut self) -> Option<T> {
-        self.head.take().map(|old_head|{
-            match old_head.borrow_mut().next.take() {
-                Some(new_head) => {
-                    new_head.borrow_mut().prev.take();
-                    self.head = Some(new_head);
-                }
-                None => {
-                    // empty list
-                    self.tail.take();
-                }
-            }
-            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
-        })
-    }
-    // this is really difficult to understand
-    // if we try to borrow the value from RefCells, got lost as it must need to return a reference to node
-    // but because of lifetime defined in fn borrow<'a>(&'a self) -> Ref<'a, T>
-    // the Ref is kept until return 'a borrowed value and once executed, the lifetime ends and then the Ref got out of scope
-    // so the solution is to create a new Ref, transforming from &T to &U, keeping the borrow referencing
-    // this is really nuts
-    pub fn peek_front(&self) -> Option<Ref<T>> {
-        self.head.as_ref().map(|node| {
-            Ref::map(node.borrow(), |node| &node.elem)
-        })
-    }
 
-    pub fn into_iter(self) -> IntoIter<T>{
+    pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
-    pub fn iter(&self) -> Iter<T> {
-        Iter(self.head.as_ref().map(|head| head.borrow()))
-    }
 }
+
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         while self.pop_front().is_some() {}
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
-    fn next(&mut self)-> Option<Self::Item> {
+
+    fn next(&mut self) -> Option<T> {
         self.0.pop_front()
     }
 }
-// getting from two sides
+
 impl<T> DoubleEndedIterator for IntoIter<T> {
-    fn next_back(&mut self) -> Option<Self::Item> {
+    fn next_back(&mut self) -> Option<T> {
         self.0.pop_back()
     }
 }
@@ -152,34 +145,6 @@ impl<T> DoubleEndedIterator for IntoIter<T> {
 mod test {
     use super::List;
 
-    #[test]
-    fn fourth_test() {
-        let mut list = List::new();
-
-        // Check empty list behaves right
-        assert_eq!(list.pop_front(), None);
-
-        // Populate list
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
-
-        // Check normal removal
-        assert_eq!(list.pop_front(), Some(3));
-        assert_eq!(list.pop_front(), Some(2));
-
-        // Push some more just to make sure nothing's corrupted
-        list.push_front(4);
-        list.push_front(5);
-
-        // Check normal removal
-        assert_eq!(list.pop_front(), Some(5));
-        assert_eq!(list.pop_front(), Some(4));
-
-        // Check exhaustion
-        assert_eq!(list.pop_front(), Some(1));
-        assert_eq!(list.pop_front(), None);
-    }
     #[test]
     fn basics() {
         let mut list = List::new();
@@ -239,11 +204,6 @@ mod test {
     fn peek() {
         let mut list = List::new();
         assert!(list.peek_front().is_none());
-        list.push_front(1); list.push_front(2); list.push_front(3);
-
-        assert_eq!(&*list.peek_front().unwrap(), &3);
-        let mut list = List::new();
-        assert!(list.peek_front().is_none());
         assert!(list.peek_back().is_none());
         assert!(list.peek_front_mut().is_none());
         assert!(list.peek_back_mut().is_none());
@@ -255,6 +215,7 @@ mod test {
         assert_eq!(&*list.peek_back().unwrap(), &1);
         assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
     }
+
     #[test]
     fn into_iter() {
         let mut list = List::new();
--- languages/rust/linked_list/src/lib/mod.rs
+++ languages/rust/linked_list/src/lib/mod.rs
@@ -2,3 +2,4 @@ pub mod first;
 pub mod second;
 mod third;
 mod fourth;
+mod fifth;
