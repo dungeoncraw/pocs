1b2d8683ad97651ecad5db76bf6a2a11947c3642
Thiago Nascimento <dungeoncraw@gmail.com>
2025-04-14 19:05:33 -0300

    Relation table and unit test

--- languages/scala/db/slick/slickpoc/src/main/scala/Tables.scala
+++ languages/scala/db/slick/slickpoc/src/main/scala/Tables.scala
@@ -11,3 +11,14 @@ class Provider(tag: Tag) extends Table[(Int, String, String, String, String, Str
   // Every table needs a * projection with the same type as the table's type parameter
   def * : ProvenShape[(Int, String, String, String, String, String)] = (id, name, street, city, state, zip)
 }
+
+class Coffees(tag: Tag) extends Table[(String, Int, Double, Int, Int)](tag, "COFFEES") {
+  def name: Rep[String] = column[String]("NAME")
+  def providerId: Rep[Int] = column[Int]("PROVIDER_ID")
+  def price: Rep[Double] = column[Double]("PRICE")
+  def sales: Rep[Int] = column[Int]("SALES")
+  def total: Rep[Int] = column[Int]("TOTAL")
+  def * : ProvenShape[(String, Int, Double, Int, Int)] = (name, providerId, price, sales, total)
+
+  def provider = foreignKey("PROVIDER_FK", providerId, TableQuery[Provider])(_.id)
+}
--- languages/scala/db/slick/slickpoc/src/main/scala/main.scala
+++ languages/scala/db/slick/slickpoc/src/main/scala/main.scala
@@ -11,13 +11,22 @@ def main(): Unit =
   val db = Database.forConfig("h2mem")
   try {
     val providers: TableQuery[Provider] = TableQuery[Provider]
+    val coffees: TableQuery[Coffees] = TableQuery[Coffees]
     val setup: DBIO[Unit] = DBIO.seq(
-      providers.schema.create,
+      (providers.schema ++ coffees.schema).create,
       providers += (101, "Acme", "123 Main St", "Anytown", "CA", "90210" ),
       providers += (102, "Nops", "456 Main St", "Anytown", "CA", "90210")
     )
     val setupFuture: Future[Unit] = db.run(setup)
     val f = setupFuture.flatMap{ _ =>
+      val insertAction: DBIO[Option[Int]] = coffees ++= Seq(
+        ( "Latte", 101, 2.50, 100, 250),
+        ("Colombian", 102, 3.00, 100, 300),
+        ("Espresso", 102, 1.50, 100, 150),
+        ("Cappuccino", 101, 2.00, 100, 200),
+        ("French_Roast", 102, 2.50, 100, 250),
+      )
+      db.run(insertAction)
       val filterQuery: Query[Provider, (Int, String, String, String, String, String), Seq] =
         // === is slick DSL for SQL equality
         providers.filter(_.zip === "90210")
