08317d9e1869a80cda6401f0805b2abfbaab391e
Thiago Nascimento <dungeoncraw@gmail.com>
2025-04-17 21:21:33 -0300

    Unsafe list

--- languages/rust/linked_list/src/lib/fifth.rs
+++ languages/rust/linked_list/src/lib/fifth.rs
@@ -1,3 +1,5 @@
+use std::ptr;
+
 pub struct List<T> {
     head: Link<T>,
     tail: *mut Node<T>, // DANGER DANGER
@@ -12,38 +14,36 @@ struct Node<T> {
 
 impl<T> List<T> {
     pub fn new() -> Self {
-        List { head: None, tail: None }
+        // this creates a null pointer :(
+        List { head: None, tail: ptr::null_mut() }
     }
 
     pub fn push(&mut self, elem: T) {
-        let new_tail = Box::new(Node {
+        let mut new_tail = Box::new(Node {
             elem: elem,
-            // When you push onto the tail, your next is always None
             next: None,
         });
 
-        // Put the box in the right place, and then grab a reference to its Node
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                // If the old tail existed, update it to point to the new tail
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
-            }
-            None => {
-                // Otherwise, update the head to point to it
-                self.head = Some(new_tail);
-                self.head.as_deref_mut()
-            }
-        };
+        let raw_tail: *mut _ = &mut *new_tail;
 
-        self.tail = new_tail;
+        if !self.tail.is_null() {
+            // force a unsafe result in a safe function
+            unsafe {
+                (*self.tail).next = Some(new_tail);
+            }
+        } else {
+            self.head = Some(new_tail);
+        }
+
+        self.tail = raw_tail;
     }
-    pub fn pop(&'a mut self) -> Option<T> {
-        self.head.take().map(|head | {
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
             let head = *head;
             self.head = head.next;
+
             if self.head.is_none() {
-                self.tail = None;
+                self.tail = ptr::null_mut();
             }
             head.elem
         })
@@ -57,28 +57,29 @@ mod test {
     fn basics() {
         let mut list = List::new();
 
-        // Check empty list behaves right
         assert_eq!(list.pop(), None);
 
-        // Populate list
         list.push(1);
         list.push(2);
         list.push(3);
 
-        // Check normal removal
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), Some(2));
 
-        // Push some more just to make sure nothing's corrupted
         list.push(4);
         list.push(5);
 
-        // Check normal removal
         assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(4));
 
-        // Check exhaustion
         assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        list.push(6);
+        list.push(7);
+
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
     }
 }
