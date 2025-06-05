85083323da98d7a1c6f0db23ae7759db29622bea
Thiago Nascimento <dungeoncraw@gmail.com>
2025-06-04 21:56:49 -0300

    using resource builder for UI

--- languages/c/netrunning/src/main.c
+++ languages/c/netrunning/src/main.c
@@ -5,49 +5,53 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
+static void quit_cb (GtkWindow *window)
+{
+    gtk_window_close (window);
+}
+
 static void print_net (GtkWidget *widget, gpointer   data)
 {
     g_print ("New net\n");
 }
 
+static void print_deck (GtkWidget *widget, gpointer   data)
+{
+    g_print ("New deck\n");
+}
+
 static void activate (GtkApplication *app,gpointer        user_data)
 {
-    GtkWidget *window;
-    GtkWidget *new_net_button;
-    GtkWidget *new_profile_button;
-    GtkWidget *grid;
+    GtkBuilder *builder = gtk_builder_new ();
+    gtk_builder_add_from_file (builder, "src/builder.ui", NULL);
+    // get reference from builder.ui for the tree object
+    GObject *window = gtk_builder_get_object (builder, "window");
+    gtk_window_set_application(GTK_WINDOW(window), app);
 
-    /* set the main window */
-    window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Netrunning tool");
-    gtk_window_set_default_size (GTK_WINDOW (window), WINDOW_WIDTH, WINDOW_HEIGHT);
+    // get the reference for button and add signal for logic
+    GObject *button = gtk_builder_get_object (builder, "generateNet");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_net), NULL);
 
-    /* grid component*/
-    grid = gtk_grid_new ();
-    gtk_window_set_child (GTK_WINDOW (window), grid);
+    button = gtk_builder_get_object (builder, "generateDeck");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_deck), NULL);
 
-    new_net_button = gtk_button_new_with_label ("Generate new net");
-    g_signal_connect (new_net_button, "clicked", G_CALLBACK (print_net), NULL);
+    button = gtk_builder_get_object (builder, "quit");
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(quit_cb), window);
 
-    /* positioning the button on the grid*/
-    gtk_grid_attach (GTK_GRID (grid), new_net_button, 0, 0, 1, 1);
+    gtk_widget_set_visible(GTK_WIDGET(window), TRUE);
 
-    new_profile_button = gtk_button_new_with_label ("Generate new profile");
-    g_signal_connect (new_profile_button, "clicked", G_CALLBACK (print_net), NULL);
-    gtk_grid_attach (GTK_GRID (grid), new_profile_button, 1, 0, 1, 1);
-
-    gtk_window_present (GTK_WINDOW (window));
+    g_object_unref(builder);
 }
 
 int main (int argc, char **argv)
 {
-    GtkApplication *app;
-    int status;
+    #ifdef GTK_SRCDIR
+        g_chdir (GTK_SRCDIR);
+    #endif
 
-    app = gtk_application_new ("com.tetokeguii.netrunning", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *app = gtk_application_new ("com.tetokeguii.netrunning", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
+    const int status = g_application_run (G_APPLICATION (app), argc, argv);
     g_object_unref (app);
-
     return status;
 }
