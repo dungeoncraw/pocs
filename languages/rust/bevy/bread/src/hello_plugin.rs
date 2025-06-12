c46f84dac9fd677d3f50d708dbe06ce4794a0c51
Thiago Nascimento <dungeoncraw@gmail.com>
2025-06-12 20:21:56 -0300

    Timer on bevy

--- languages/rust/bevy/bread/src/hello_plugin.rs
+++ languages/rust/bevy/bread/src/hello_plugin.rs
@@ -7,18 +7,20 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Don Proctor".to_string())));
     commands.spawn((Person, Name("Enzo Um".to_string())));
     commands.spawn((Person, Name("Naty Bleu".to_string())));
 }
-fn hello_world() {
-    println!("hello world!");
-}
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -33,9 +35,10 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
         // .chain make sure the order for update_people and greet_people happens, otherwise can greet
         // and name change after
-        app.add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+        app.add_systems(Update,  (update_people, greet_people).chain());
     }
 }
