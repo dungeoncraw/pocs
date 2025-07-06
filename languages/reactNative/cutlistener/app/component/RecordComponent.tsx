35f9514a1603397be1a5e7260f938db31a0a392d
Thiago Nascimento <dungeoncraw@gmail.com>
2025-07-06 08:35:54 -0300

    Refactor to use a class and start new plugin

--- languages/reactNative/cutlistener/app/component/RecordComponent.tsx
+++ languages/reactNative/cutlistener/app/component/RecordComponent.tsx
@@ -1,12 +1,5 @@
 import {Button, Text, SafeAreaView, Platform, StyleSheet} from "react-native";
-import AudioRecorderPlayer, {
-    AudioEncoderAndroidType,
-    AudioSet,
-    AudioSourceAndroidType,
-    AVEncoderAudioQualityIOSType, AVEncodingOption, OutputFormatAndroidType, PlayBackType, RecordBackType
-} from "react-native-audio-recorder-player";
-import {useState} from "react";
-import {requestMicrophonePermission} from "@/app/helper/requestPermission";
+import RecordProvider from "@/app/helper/recordProvider";
 
 export enum PluginType {
     REACT_NATIVE_AUDIO_RECORDER_PLAYER = "RECORDER_PLAYER",
@@ -19,85 +12,21 @@ type IProps = {
 }
 
 export default function RecordComponent({text, pluginType}: IProps) {
-    const [recordSecs, setRecordSecs] = useState<{ recordSecs: number, recordTime: string }>({
-        recordSecs: 0,
-        recordTime: '00:00'
-    });
-    const [playbackMeta, setPlaybackMeta] = useState<{
-        currentPositionSec: number,
-        currentDurationSec: number,
-        playTime: string,
-        duration: string
-    }>({currentPositionSec: 0, currentDurationSec: 0, playTime: '00:00', duration: '00:00'});
-
-    // THIS NEEDS TO BE REFACTORED TO HANDLE NEW PLUGIN
-    const audioRecordPLayer = new AudioRecorderPlayer();
     const path = Platform.select({
         ios: undefined,
         android: undefined
     });
+    const audioRecordPLayer = new RecordProvider(pluginType, path);
+
     const onPressRecord = async () => {
-        if (pluginType === PluginType.REACT_NATIVE_AUDIO_RECORDER_PLAYER) {
-            if (Platform.OS === 'android') {
-                try {
-                    const hasPermissions = await requestMicrophonePermission();
-
-                    if (!hasPermissions) {
-                        console.log('Permission not granted');
-                        return;
-                    }
-                } catch (err) {
-                    console.warn(err);
-
-                    return;
-                }
-            }
-
-            const audioSet: AudioSet = {
-                AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
-                AudioSourceAndroid: AudioSourceAndroidType.MIC,
-                AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
-                AVNumberOfChannelsKeyIOS: 2,
-                AVFormatIDKeyIOS: AVEncodingOption.aac,
-                OutputFormatAndroid: OutputFormatAndroidType.AAC_ADTS,
-            };
-
-            console.log('audioSet', audioSet);
-            const uri = await audioRecordPLayer.startRecorder(
-                path,
-                audioSet,
-            );
-
-            audioRecordPLayer.addRecordBackListener((e: RecordBackType) => {
-                setRecordSecs({
-                    recordSecs: e.currentPosition,
-                    recordTime: audioRecordPLayer.mmssss(
-                        Math.floor(e.currentPosition),
-                    ),
-                });
-            });
-            console.log(`uri: ${uri}`);
-        }
+        await audioRecordPLayer.onRecord();
     };
 
     const onPressStop = async () => {
-        const result = await audioRecordPLayer.stopRecorder();
-        audioRecordPLayer.removeRecordBackListener();
-        setRecordSecs({...recordSecs, recordSecs: 0});
-        console.log(`result: ${result}`);
+        await audioRecordPLayer.onStop();
     }
     const onPressPlay = async () => {
-        const msg = await audioRecordPLayer.startPlayer(path);
-        const volume = await audioRecordPLayer.setVolume(1.0);
-        console.log(`msg: ${msg}, volume: ${volume}`);
-        audioRecordPLayer.addPlayBackListener((e: PlayBackType) => {
-            setPlaybackMeta({
-                currentPositionSec: e.currentPosition,
-                currentDurationSec: e.duration,
-                playTime: audioRecordPLayer.mmssss((Math.floor(e.currentPosition))),
-                duration: audioRecordPLayer.mmssss((Math.floor(e.duration))),
-            });
-        });
+        await audioRecordPLayer.onPlay();
     }
     return (
         <SafeAreaView style={styles.component}>
---
+++ languages/reactNative/cutlistener/app/helper/recordProvider.ts
@@ -0,0 +1,104 @@
+import {PluginType} from "@/app/component/RecordComponent";
+import AudioRecorderPlayer, {
+    AudioEncoderAndroidType,
+    AudioSet,
+    AudioSourceAndroidType,
+    AVEncoderAudioQualityIOSType,
+    AVEncodingOption,
+    OutputFormatAndroidType,
+    PlayBackType,
+    RecordBackType
+} from "react-native-audio-recorder-player";
+import {createAudioPlayer} from "expo-audio";
+import {Platform} from "react-native";
+import {requestMicrophonePermission} from "@/app/helper/requestPermission";
+
+class RecordProvider {
+    pluginType: PluginType;
+    recordPLugin: any;
+    path: string | undefined;
+    recordSecs = 0;
+    recordTime = '00:00';
+    currentPositionSec= 0;
+    currentDurationSec= 0;
+    playTime=  '00:00';
+    duration= '00:00';
+    constructor(pluginType: PluginType, path: string | undefined) {
+        this.pluginType = pluginType;
+        this.path = path;
+        switch (pluginType) {
+            case PluginType.REACT_NATIVE_AUDIO_RECORDER_PLAYER:
+                this.recordPLugin = new AudioRecorderPlayer();
+                break;
+            case PluginType.EXPO_AUDIO:
+                this.recordPLugin = createAudioPlayer();
+        }
+    }
+
+    async onRecord() {
+        if (this.pluginType === PluginType.REACT_NATIVE_AUDIO_RECORDER_PLAYER) {
+            if (Platform.OS === 'android') {
+                try {
+                    const hasPermissions = await requestMicrophonePermission();
+
+                    if (!hasPermissions) {
+                        console.log('Permission not granted');
+                        return;
+                    }
+                } catch (err) {
+                    console.warn(err);
+                    return;
+                }
+            }
+
+            const audioSet: AudioSet = {
+                AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
+                AudioSourceAndroid: AudioSourceAndroidType.MIC,
+                AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
+                AVNumberOfChannelsKeyIOS: 2,
+                AVFormatIDKeyIOS: AVEncodingOption.aac,
+                OutputFormatAndroid: OutputFormatAndroidType.AAC_ADTS,
+            };
+
+            console.log('audioSet', audioSet);
+            const uri = await this.recordPLugin.startRecorder(
+                this.path,
+                audioSet,
+            );
+
+            this.recordPLugin.addRecordBackListener((e: RecordBackType) => {
+                    this.recordSecs = e.currentPosition;
+                    this.recordTime = this.recordPLugin.mmssss(
+                        Math.floor(e.currentPosition),
+                    );
+            });
+            console.log(`uri: ${uri}`);
+        }
+    }
+
+    async onStop() {
+        if (this.pluginType === PluginType.REACT_NATIVE_AUDIO_RECORDER_PLAYER) {
+            const result = await this.recordPLugin.stopRecorder();
+            this.recordPLugin.removeRecordBackListener();
+            this.recordSecs = 0;
+            console.log(`result: ${result}`);
+        }
+    }
+
+    async onPlay() {
+        if (this.pluginType === PluginType.REACT_NATIVE_AUDIO_RECORDER_PLAYER) {
+            const msg = await this.recordPLugin.startPlayer(this.path);
+            const volume = await this.recordPLugin.setVolume(1.0);
+            console.log(`msg: ${msg}, volume: ${volume}`);
+            this.recordPLugin.addPlayBackListener((e: PlayBackType) => {
+                    this.currentPositionSec = e.currentPosition;
+                    this.currentDurationSec =  e.duration;
+                    this.playTime = this.recordPLugin.mmssss((Math.floor(e.currentPosition)));
+                    this.duration = this.recordPLugin.mmssss((Math.floor(e.duration)));
+            });
+        }
+    }
+
+}
+
+export default RecordProvider;
