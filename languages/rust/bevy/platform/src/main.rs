fecb3632f2bc2a8b4bfeb02a4298007831b3efb9
Thiago Nascimento <dungeoncraw@gmail.com>
2025-07-29 20:27:53 -0300

    Basic sprite animation, still need a better sample

--- languages/rust/bevy/platform/src/main.rs
+++ languages/rust/bevy/platform/src/main.rs
@@ -1,30 +1,136 @@
-// src/main.rs
-mod player;
-mod camera;
+use std::time::Duration;
 
-use bevy::prelude::*;
-use player::*;
-use camera::*;
+use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Contra Clone".into(),
-                resolution: (800., 480.).into(),
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_startup_system(setup)
-        .add_systems(Update, (player_movement, follow_player))
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
+        .add_systems(Startup, setup)
+        .add_systems(Update, execute_animations)
+        .add_systems(
+            Update,
+            (
+                // Press the right arrow key to animate the right sprite
+                trigger_animation::<RightSprite>.run_if(input_just_pressed(KeyCode::ArrowRight)),
+                // Press the left arrow key to animate the left sprite
+                trigger_animation::<LeftSprite>.run_if(input_just_pressed(KeyCode::ArrowLeft)),
+            ),
+        )
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Câmera
-    commands.spawn(Camera2dBundle::default());
+fn trigger_animation<S: Component>(mut animation: Single<&mut AnimationConfig, With<S>>) {
+    // We create a new timer when the animation is triggered
+    animation.frame_timer = AnimationConfig::timer_from_fps(animation.fps);
+}
 
-    // Player inicial
-    spawn_player(&mut commands, &asset_server);
+#[derive(Component)]
+struct AnimationConfig {
+    first_sprite_index: usize,
+    last_sprite_index: usize,
+    fps: u8,
+    frame_timer: Timer,
+}
+
+impl AnimationConfig {
+    fn new(first: usize, last: usize, fps: u8) -> Self {
+        Self {
+            first_sprite_index: first,
+            last_sprite_index: last,
+            fps,
+            frame_timer: Self::timer_from_fps(fps),
+        }
+    }
+
+    fn timer_from_fps(fps: u8) -> Timer {
+        Timer::new(Duration::from_secs_f32(1.0 / (fps as f32)), TimerMode::Once)
+    }
+}
+
+// This system loops through all the sprites in the `TextureAtlas`, from `first_sprite_index` to
+// `last_sprite_index` (both defined in `AnimationConfig`).
+fn execute_animations(time: Res<Time>, mut query: Query<(&mut AnimationConfig, &mut Sprite)>) {
+    for (mut config, mut sprite) in &mut query {
+
+        config.frame_timer.tick(time.delta());
+
+        if config.frame_timer.just_finished() {
+            if let Some(atlas) = &mut sprite.texture_atlas {
+                if atlas.index == config.last_sprite_index {
+                    atlas.index = config.first_sprite_index;
+                } else {
+                    atlas.index += 1;
+                    config.frame_timer = AnimationConfig::timer_from_fps(config.fps);
+                }
+            }
+        }
+    }
+}
+
+#[derive(Component)]
+struct LeftSprite;
+
+#[derive(Component)]
+struct RightSprite;
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    commands.spawn(Camera2d);
+
+    // Create a minimal UI explaining how to interact with the example
+    commands.spawn((
+        Text::new("Left Arrow: Animate Left Sprite\nRight Arrow: Animate Right Sprite"),
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(12.0),
+            left: Val::Px(12.0),
+            ..default()
+        },
+    ));
+
+    // Ok this is a not ready sample, but is what can be found
+    let texture = asset_server.load("character/running_sample.png");
+
+    // The sprite sheet has 6 sprites arranged in a row, and they are all 32px x 32px
+    let layout = TextureAtlasLayout::from_grid(UVec2::splat(32), 6, 1, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    // The first (left-hand) sprite runs at 10 FPS
+    let animation_config_1 = AnimationConfig::new(1, 5, 10);
+
+    // Create the first (left-hand) sprite
+    commands.spawn((
+        Sprite {
+            image: texture.clone(),
+            texture_atlas: Some(TextureAtlas {
+                layout: texture_atlas_layout.clone(),
+                index: animation_config_1.first_sprite_index,
+            }),
+            ..default()
+        },
+        Transform::from_scale(Vec3::splat(6.0)).with_translation(Vec3::new(-70.0, 0.0, 0.0)),
+        LeftSprite,
+        animation_config_1,
+    ));
+
+    // The second (right-hand) sprite runs at 20 FPS
+    let animation_config_2 = AnimationConfig::new(1, 5, 20);
+
+    // Create the second (right-hand) sprite
+    commands.spawn((
+        Sprite {
+            image: texture.clone(),
+            texture_atlas: Some(TextureAtlas {
+                layout: texture_atlas_layout.clone(),
+                index: animation_config_2.first_sprite_index,
+            }),
+            ..Default::default()
+        },
+        Transform::from_scale(Vec3::splat(6.0)).with_translation(Vec3::new(70.0, 0.0, 0.0)),
+        RightSprite,
+        animation_config_2,
+    ));
 }
