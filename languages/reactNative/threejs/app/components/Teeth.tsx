a853ec3aab9877ae7323c02822cb0557c1160734
Thiago Nascimento <dungeoncraw@gmail.com>
2025-09-02 19:35:00 -0300

    Clone asset and rotate every frame

--- languages/reactNative/threejs/app/components/Teeth.tsx
+++ languages/reactNative/threejs/app/components/Teeth.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from 'three';
 
 export default function Teeth() {
-    const { scene, nodes, materials } = useGLTF('/assets/models/free_teeth_base_mesh.glb') as any;
+    const { scene } = useGLTF(require('../../assets/models/teeth.glb'));
+    const groupRef = useRef<THREE.Group>(null);
 
+    useEffect(() => {
+        if (groupRef.current) {
+            groupRef.current.position.set(0.6, -1, 0);
+        }
+    }, []);
+    // useFrame is a hook that allows you to run code on every frame
+    // delta is the time elapsed since the last frame
+    useFrame((_, delta) => {
+        if (groupRef.current) {
+            groupRef.current.rotation.y += delta;
+        }
+    });
     if (!scene) return null;
-    console.log(scene);
-    console.log(nodes);
-    console.log(materials);
+
     return (
         <>
-            <primitive object={nodes.Cube001} position={[0, 0, 0]} scale={[0.3, 0.3, 0.3]}/>
-            <primitive object={nodes.Cube002} position={[2, 0, 0]} scale={[0.3, 0.3, 0.3]}/>
-            <primitive object={nodes.Cube003} position={[-2, 0, 0]} scale={[0.3, 0.3, 0.3]}/>
+            {/*rotation x, y, z */}
+            <group position={[-1.2, 2, -0.9]} rotation={[0.3, 0.2, 0]}>
+                <primitive object={scene} />
+            </group>
+            <group ref={groupRef}>
+                {/* clone is needed to avoid the model being duplicated, reusing the same glb file*/}
+                <primitive object={scene.clone()}/>
+            </group>
         </>
+
     );
 }
