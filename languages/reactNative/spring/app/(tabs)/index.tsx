01736926f716d5616272321c93ab1c0ad7c7212b
Thiago Nascimento <dungeoncraw@gmail.com>
2025-09-09 08:07:52 -0300

    Understanding Symbol as it's referenced on source code of react spring

--- languages/reactNative/spring/app/(tabs)/index.tsx
+++ languages/reactNative/spring/app/(tabs)/index.tsx
@@ -1,6 +1,13 @@
 import {StyleSheet, View} from 'react-native';
 
 import {useSpring, animated} from "@react-spring/native";
+// https://medium.com/@robinviktorsson/understanding-symbols-in-typescript-a-deep-dive-with-practical-examples-82011a838783
+class Person {
+    constructor(public name: string) {}
+    get[Symbol.toStringTag]() {
+        return `${this.name} is a person.`;
+    }
+}
 
 export default function HomeScreen() {
   const springs = useSpring({
@@ -8,6 +15,11 @@ export default function HomeScreen() {
       to: { color: 'blue' },
       config: { duration: 1000 },
   });
+  const person = new Person('John');
+  // This will print "[object Object is a person.]" using Symbol.toStringTag from TS
+  // pretty much is for debugging purposes
+  console.log(Object.prototype.toString.call(person));
+
   return (
       <View style={styles.container}>
           <animated.Text
