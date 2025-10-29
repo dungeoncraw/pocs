94cca88c0e1a7e0fb96fe843883e2b3860fe5cb0
Thiago Nascimento <dungeoncraw@gmail.com>
2025-10-28 21:39:49 -0300

    Upper bounds for generic type

--- languages/scala/30days/day10/src/main/scala/main.scala
+++ languages/scala/30days/day10/src/main/scala/main.scala
@@ -6,6 +6,27 @@ class Box[T](value: T) {
 def identity[T](x: T): T = x
 def swap[A, B](pair: (A, B)): (B, A) = (pair._2, pair._1)
 
+abstract class Animal {
+  def name: String
+}
+
+abstract class Pet extends Animal
+
+class Cat extends Pet {
+  def name: String = "Cat"
+}
+class Dog extends Pet {
+  def name: String = "Dog"
+}
+
+class Lion extends Animal {
+  def name: String = "Lion"
+}
+
+class PetContainer[P <:Pet](p:P):
+  def pet: P = p
+
+
 @main
 def main(): Unit = {
   val stringBox = Box("Hello")
@@ -20,5 +41,10 @@ def main(): Unit = {
   println(swapped)
   println(result1)
   println(result2)
+  val dogContainer = PetContainer[Dog](Dog())
+  val catContainer = PetContainer[Cat](Cat())
+  //  the next line fails as the generic type is not a subtype defined on upper bound
+  //  val lionContainer = PetContainer[Lion](Lion())
+
 }
 
