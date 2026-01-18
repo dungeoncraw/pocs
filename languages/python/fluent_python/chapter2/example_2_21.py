ffadad9cea99906517b8fc56d3f8e04bc637a88b
Thiago Nascimento <dungeoncraw@gmail.com>
2026-01-18 17:59:19 -0300

    Fluent: example 2.20 and 2.21

---
+++ languages/python/fluent_python/chapter2/example_2_21.py
@@ -0,0 +1,12 @@
+import array
+
+if __name__ == '__main__':
+    numbers = array.array('h', [-2, -1, 0, 1, 2])
+    print(numbers)
+    memv = memoryview(numbers)
+    print(len(memv))
+    print(memv[0])
+    memv_oct = memv.cast('B')
+    print(memv_oct.tolist())
+    memv_oct[5] = 4
+    print(numbers)
