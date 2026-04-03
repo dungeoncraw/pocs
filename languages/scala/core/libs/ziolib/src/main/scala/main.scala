2e12904cd8bb0dff1d2327a28b88976b913c3cf2
Thiago Nascimento <dungeoncraw@gmail.com>
2026-04-03 18:40:51 -0300

    Scala: zio ref parallel compute

--- languages/scala/core/libs/ziolib/src/main/scala/main.scala
+++ languages/scala/core/libs/ziolib/src/main/scala/main.scala
@@ -1,21 +1,17 @@
 import zio.*
 import scala.collection.mutable.ListBuffer
 
-@main
-def run(): Unit =
-  val tasks = ListBuffer.empty[String]
+object Main extends ZIOAppDefault:
+  def run: ZIO[ZIOAppArgs & Scope, Any, Any] =
+    val tasks = ListBuffer.empty[String]
 
-  val program =
     for
       _ <- Console.printLine("Enter tasks one per line. Type 'done' to finish.")
       _ <- addTasks(tasks)
       _ <- Console.printLine(s"You entered ${tasks.size} task(s):")
       _ <- ZIO.foreachDiscard(tasks.toList)(task => Console.printLine(s"- $task"))
+      _ <- computeParallel()
     yield ()
-
-  Unsafe.unsafe { implicit unsafe =>
-    Runtime.default.unsafe.run(program).getOrThrowFiberFailure()
-  }
 
 def addTasks(tasks: ListBuffer[String]): ZIO[Any, Throwable, Unit] =
   for
@@ -28,4 +24,23 @@ def addTasks(tasks: ListBuffer[String]): ZIO[Any, Throwable, Unit] =
       else
         tasks += input.trim
         Console.printLine(s"Added: ${input.trim}") *> addTasks(tasks)
+  yield ()
+
+def computeParallel(): ZIO[Any, Throwable, Unit] =
+  for
+    // create a mutable ref to update in multiple fibers
+    ref <- Ref.make(0)
+    // create 100 fibers that update the ref 1000 times each
+    fibers <- ZIO.foreachPar(1 to 100) { _ =>
+      ZIO.foreachDiscard(1 to 1000) { _ =>
+        // update the ref
+        ref.update(_ + 1)
+      }
+      // fork to start the computation in parallel
+    }.fork
+    // just wait the fibers to finish
+    _ <- fibers.join
+    //get the final value 100000
+    value <- ref.get
+    _ <- Console.printLine(s"Final value: $value")
   yield ()
