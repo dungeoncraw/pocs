9552d8cf6b386fd66f61e5bee1b657df5b0a25a2
Thiago Nascimento <dungeoncraw@gmail.com>
2026-04-27 21:46:46 -0300

    Godot: frpg crop data

---
+++ context/games/godot/frpg/Scripts/crop_data.gd
@@ -0,0 +1,17 @@
+class_name CropData
+extends Resource
+
+@export var growth_sprites: Array[Texture]
+@export var days_to_growth: int = 8
+@export var seed_price: int = 10
+@export var sell_price: int = 20
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
