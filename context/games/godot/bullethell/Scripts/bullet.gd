dfe761650095970935e3871817b1fb05ae20d313
Thiago Nascimento <dungeoncraw@gmail.com>
2026-05-03 20:39:07 -0300

    Godot: hell shooting player

---
+++ context/games/godot/bullethell/Scripts/bullet.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@export var speed: float = 200.0
+@export var owner_group: String
+@onready var destroy_timer: Timer = $DestroyTimer
+
+var move_dir: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	translate(move_dir * speed * delta)
+	rotation = move_dir.angle()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_destroy_timer_timeout() -> void:
+	queue_free()
--- context/games/godot/bullethell/Scripts/player.gd
+++ context/games/godot/bullethell/Scripts/player.gd
@@ -1,25 +1,46 @@
 extends CharacterBody2D
 
+@export var max_speed: float = 100.0
+@export var acceleration: float = 0.2
+@export var braking: float = 0.15
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+@onready var sprite: Sprite2D = $Sprite
+@onready var muzzle: Node2D = $Muzzle
+# 0.1 s can  shoot a bullet
+@export var shoot_rate: float = 0.1
+
+var last_shoot_time: float
+var move_input: Vector2
+
+#temp
+var bullet_scene: PackedScene = preload("res://Scenes/bullet.tscn")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	move_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if move_input.length() > 0:
+		#	increase by acceleration
+		velocity = velocity.lerp(move_input * max_speed, acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# braking until zero
+		velocity = velocity.lerp(Vector2.ZERO, braking)
+	move_and_slide()	
 
-	move_and_slide()
+func _process(delta: float) -> void:
+	sprite.flip_h = get_global_mouse_position().x > global_position.x
+	
+	if Input.is_action_pressed("shoot"):
+		if Time.get_unix_time_from_system() - last_shoot_time > shoot_rate:
+			_shoot()
+
+func _shoot():
+	last_shoot_time = Time.get_unix_time_from_system()
+	var bullet = bullet_scene.instantiate()
+	get_tree().root.add_child(bullet)
+	
+	bullet.global_position = muzzle.global_position
+	var mouse_pos = get_global_mouse_position()
+	var mouse_dir = muzzle.global_position.direction_to(mouse_pos)
+	
+	bullet.move_dir = mouse_dir
